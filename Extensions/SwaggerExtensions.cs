dc3ea8b86a3f47945e0a77b9bf8f9f2051e93ee5
Théo Etaix <tetaix@MacBook-Pro-de-Theo.local>
2021-01-15 14:04:15 +0100

    ADD Swagger

---
+++ Extensions/SwaggerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace tetaix_circuit.Extensions
+{
+    public class SwaggerExtensions
+    {
+        public SwaggerExtensions()
+        {
+        }
+    }
+}
---
+++ Repository/IPackRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using tetaix_circuit.Modele;
+
+namespace Api.Repositories
+{
+    public interface ICircuitRepository
+    {
+        Task<List<Circuit>> GetCircuit();
+        Task<Circuit> GetCircuitById(int id);
+        Task CreateCircuit(Circuit Circuit);
+        Task UpdateCircuit(Circuit Circuit);
+        Task DeleteCircuit(int id);
+    }
+}
